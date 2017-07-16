71467beb09107c05b66a88bf34b5a11487711fc9
veldymanov <veldymanov@gmail.com>
2017-07-16 14:13:15 +0300

    inter-iframe

---
+++ script/jquery/jqIframeApp.js
@@ -0,0 +1,58 @@
+jQuery(document).ready(function(){
+	//Define targetOrigin
+	var domain = "*"; //"http://localhost:3000";
+	
+	/************************************
+	* Send message about iframe creation
+	*************************************/
+	var iframeNumber = $("#js-lbl_msg").text(); 
+	iframeNumber = iframeNumber.slice(1, iframeNumber.length - 2);
+	parent.postMessage("[system]: " + iframeNumber + " joined the conversation", domain);	
+
+	/************************************
+	* Post message
+	*************************************/
+	document.getElementById("js-form").addEventListener('submit', function(e){
+		var name = document.getElementById("js-lbl_msg").innerHTML;
+		var msg = document.getElementById("js-msg").value;
+		
+		parent.postMessage(name + ' ' + msg, domain);
+		
+		//Reset form
+		this.reset();
+			
+		e.preventDefault();
+	});
+	
+	/************************************
+	* Receive and publish message
+	*************************************/
+	window.addEventListener('message', function (e) {
+		// Message security check
+		if (e.origin !== domain){
+//			console.log("e.origin: " + e.origin);
+//			return;
+		}
+		
+		//Separete frame name from message
+		var index = e.data.indexOf(':');
+		var frameName = e.data.slice(0, index);
+		var msg = e.data.slice(index + 1);
+		
+		var newComment = $("<li class='comments-item'>" + 
+								"<strong>" + frameName + " - </strong>" +
+								msg + "</li>");
+			$(".comments").append(newComment);
+			
+			//Scroll to bottom
+			var ulList = document.getElementById("js-comments");
+			ulList.scrollTop = ulList.scrollHeight;
+		});	
+		
+	/************************************
+	* Set focus on input field
+	*************************************/
+	window.addEventListener('click', function () {
+		$("#js-msg").focus();	
+	});
+});
---
+++ script/jquery/jqueryApp.js
@@ -0,0 +1,152 @@
+/*--------------------------------------------------
+*	Iframe chat object
+--------------------------------------------------*/
+var iframeChat = {
+	
+	counter: 1,
+	
+	targetOrigin: '*', //'http://localhost:3000',	
+	
+	createFrame: function() {
+		var frameContainer = $("<div class='iframe-container js-iframe-container'>");
+		var iframe = $("<iframe class='iframe js-iframe'></iframe>");
+		var deleteFrame = $("<a href='' class='deleteFrame js-deleteFrame'>X</a>");	
+		
+		frameContainer.append(iframe);
+		frameContainer.append(deleteFrame);
+		$(".js-body").append(frameContainer);
+		
+		$(".js-iframe")[$(".js-iframe").length - 1].srcdoc = "<!DOCTYPE html>" +
+			"<html>" +
+			"<head>" +
+				"<link rel='stylesheet' href='style/normalize.css' />" +
+				"<link href='style/iframe.css' rel='stylesheet' type='text/css'/>" +
+			"</head>" +
+			"<body>" +
+				"<ul class='comments'  id='js-comments'></ul>" +
+				"<form id='js-form' class='form'>" +
+					"<label for='js-msg' id='js-lbl_msg' class='label'>[iFrame" + this.counter + "]:</label>" +
+					"<input type='text' id='js-msg' class='msg' placeholder='Write message'/>" +
+					"<input type='submit' class='submit' value='Send'/>" +
+				"</form>" +
+				"<script src='script/jquery/jquery-3.2.0.min.js'></script>" +
+				"<script src='script/jquery/jqIframeApp.js'></script>" +
+			"</body>" +
+			"</html>";
+			
+		/************************************
+		* Delete iframe-container listener
+		*************************************/
+		deleteFrame.on('mousedown', function(e){
+			e.preventDefault();	
+			iframeChat.deleteFrame( $(this) );
+		});	
+		
+		/************************************
+		* Set focus on input field listener
+		*************************************/
+		frameContainer.on('click', function(){
+			$(this).children(".js-iframe").contents().find("#js-msg").focus(); 
+		});
+		
+		/************************************
+		* iframe Drag'n'Drop event listeners
+		*************************************/	
+		//Cancell default behaviour
+		frameContainer.on("dragstart", function(e){
+			e.preventDefault();
+		});
+		
+		//iframe Drag'n'Drop
+		frameContainer.on('mousedown', function(e){
+			var elem = this;
+			var deltaX0 = parseInt($(elem).css('left'), 10) - e.pageX; 
+			var deltaY0= parseInt($(elem).css('top'), 10) - e.pageY;
+
+			$(".js-iframe-container").children(".js-iframe").css( "zIndex", -2 );
+			$(".js-iframe-container").children(".js-deleteFrame").css( "zIndex", -2 );
+			// over the rest
+			$(elem).children(".js-iframe").css( "zIndex", -1 ); 
+			$(elem).children(".js-deleteFrame").css( "zIndex", -1 ); 
+
+			function moveAt(e) {
+				$(elem).css({
+					'left': e.pageX + deltaX0 + 'px',
+					'top': e.pageY + deltaY0 + 'px'
+				});			
+			}
+
+			document.onmousemove = function(e) {
+				e.preventDefault();
+				moveAt(e);
+			};
+
+			elem.onmouseup = function() {
+				e.preventDefault();
+				document.onmousemove = null;
+				elem.onmouseup = null;	
+			};
+		});
+	
+			
+		this.counter++;
+	},
+		
+	postToAllFrames: function(msg, targetOrigin) {
+		/****************************
+		* Post message to all iframes
+		*****************************/
+		var allIframes = $(".js-iframe");	
+			
+		for (var i = 0; i < allIframes.length; i++){
+			allIframes[i].contentWindow.postMessage(msg, targetOrigin);
+		}	
+	},
+	
+	deleteFrame: function(elem) {			
+
+		//get iframe number	
+		var iframeName = $(elem).prev().contents().find("#js-lbl_msg").text(); 
+		var index = iframeName.indexOf(':');
+		var iframeName = iframeName.slice( 1, index-1 );
+		
+		//Send message about iframe deletion
+		iframeChat.postToAllFrames("[system]: " + iframeName + " left the conversation", 
+									iframeChat.targetOrigin);	
+		
+		// iframe-container (div) invisible		
+		$(elem).closest( ".js-iframe-container").css({'visibility': 'hidden'}); //.remove();	
+		$(elem).prev().css({'display': 'none'});
+	}
+};
+
+
+jQuery(document).ready(function(){
+	window.addEventListener('message', function (e) {		
+		// Message security check		
+		if (e.origin !== iframeChat.targetOrigin){
+//			console.log("e.origin: " + e.origin);
+//			return;
+		}
+		
+		//Post msg to all iframes
+		iframeChat.postToAllFrames(e.data, iframeChat.targetOrigin);	
+	});
+	
+	//Create iframe
+	$(".plus").on('click', function(){
+		iframeChat.createFrame();
+	});	
+	
+	//Delete iframe
+	$(".js-deleteFrame").on('mousedown', function(e){
+		e.preventDefault();	
+		iframeChat.deleteFrame( $(this) );
+	});
+	
+	//set focus on input field
+	$(".js-iframe-container").on('click', function(){
+		$(this).children(".js-iframe").contents().find("#js-msg").focus(); 
+	});
+	
+});
